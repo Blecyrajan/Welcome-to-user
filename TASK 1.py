4072b3f1d4e43bd921ca217b293b9256db6075ab
Blecyrajan <84286020+Blecyrajan@users.noreply.github.com>
2021-06-07 21:10:30 +0530

    Create TASK 1.py

---
+++ TASK 1.py
@@ -0,0 +1,4 @@
+celsius=float(input('Enter temperature in  degree Celcius:'))
+#calculate temperature in Fahrenheit
+fahrenheit=(celsius*1.8)+32
+print('Equivalent Fahrenheit temperature is %0.1f degree Fahrenheit'%(fahrenheit))
