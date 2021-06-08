b02e941f9da7fb40d422f553b2aa406a369f0d2b
Blecyrajan <84286020+Blecyrajan@users.noreply.github.com>
2021-06-08 19:26:47 +0530

    Update task 1.py

--- task 1.py
+++ task 1.py
@@ -1,2 +1,4 @@
-name = input(print("Enter your name: ")
-             print("Welcome",name+"!")
+celsius=float(input('Enter temperature in  degree Celsius:'))
+#calculate temperature in Fahrenheit
+fahrenheit=(celsius*1.8)+32
+print('Equivalent Fahrenheit temperature is %0.1f degree Fahrenheit'%(fahrenheit))
